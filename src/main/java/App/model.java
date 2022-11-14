e14f9c1d21ccdb5fa0279fc0454c0fbebff9e141
Tanmay Kaushik <kaushikt@tcd.ie>
2022-11-14 13:37:10 +0000

    Merged main

--- src/main/java/App/model.java
+++ src/main/java/App/model.java
@@ -1,17 +1,29 @@
 package App;
 
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
+@Data
+@Getter
+@Setter
+@ToString
 public class model {
 
-    private String docId;
-    private String date;
-    private String title;
-    private String content;
+    public String docNo;
+    public String date;
+    public String title;
+    public String content;
+
+    public model(String docNo, String date, String title, String content) {
+        this.docNo = docNo;
+        this.date = date;
+        this.title = title;
+        this.content = content;
+    }
 
 
 
 }
-
-
-
